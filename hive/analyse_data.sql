f559dc04c8b652ebb7188b501e8f3c69deaea256
ayman <aymanhabiballah84@gmail.com>
2026-06-07 02:18:35 +0100

    update  structer of project

--- hive/analyse_data.sql
+++ hive/analyse_data.sql
@@ -16,7 +16,7 @@ CREATE EXTERNAL TABLE students (
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES (
   "separatorChar" = ",",
-  "quoteChar"     = "\""
+  "quoteChar"     = "\"
 )
 STORED AS TEXTFILE
 LOCATION 'hdfs://namenode:9000/Data'
